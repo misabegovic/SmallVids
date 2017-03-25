1473cc877bdbddd3b3074bf82ab8fe6df5ddfe5f
Muhamed Isabegovic <m.isabegovic@hotmail.com>
2017-03-25 18:11:56 +0100

    random tag

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
+    redirect_to tag_path(random) if (ActsAsTaggableOn::Tag.all.length > 0 && params[:random])
     @most_used_tags = ActsAsTaggableOn::Tag.most_used(10)
     @videos = VideoPost.all
     search(params[:search][:tags]) if params[:search] && params[:search][:tags] != ''
@@ -11,6 +12,16 @@ class PagesController < ApplicationController
   def search(tags)
     tags.split(',').map do |name|
       @videos = @videos.tagged_with(name)
+    end
+  end
+
+  def random
+    random_num = Random.rand(ActsAsTaggableOn::Tag.all.length)
+    ActsAsTaggableOn::Tag.all.each do |tag|
+      if random_num < 2
+        return tag.name
+      end
+      random_num = random_num/2
     end
   end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def show
     @most_used_tags = ActsAsTaggableOn::Tag.most_used(10)
-    @video_posts = VideoPost.tagged_with(params[:id]).where(is_approved: true)
+    @video_posts = VideoPost.tagged_with(params[:id]).where(is_approved: true).first(20)
   end
 end
